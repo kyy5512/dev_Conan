4fa1bb64d4a77bca22791f0b7ed547ab1a2fe871
conan <kyy5512@hanmail.net>
2017-11-28 17:31:49 +0900

    11.28 랜딩페이지 2차수정

--- landingpage/js/custom.js
+++ landingpage/js/custom.js
@@ -1,3 +1,48 @@
+/*===============================================
+           WOW
+=================================================*/
 $(function() {
   // Handler for .ready() called.
+    new WOW().init();
+
+});
+
+/*================================================
+           Maginific Popup
+http://dimsemenov.com/plugins/magnific-popup/documentation.html
+===================================================*/
+$(function() {
+  // Handler for .ready() called.
+    $('#work').magnificPopup({
+        delegate: 'a', //child items selectior, by clicking on it popup will open
+        type: 'image' ,
+        gallery:{
+            enabled: true
+        }
+        // other option
+       })
+
+});
+
+/*$('.gallery').each(function() { // the containers for all your galleries
+    $(this).magnificPopup({
+        delegate: 'a', //the selector for gallery item
+        type: 'image',
+        gallery: {
+            enabled:true
+        }
+    });
+});*/
+
+/*================================================
+    Owl Carousel
+     http://owlcarousel2.github.io/OwlCarousel2/
+===================================================*/
+
+$(function() {
+  // Handler for .ready() called.
+   $(".owl-carousel").owlCarousel({
+        items: 3,
+   });
+
 });
